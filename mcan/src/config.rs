dfa7a32672b9ba0471a8128777ca1d16f02fc441
Gabriel Górski <gabriel.gorski@volvocars.com>
2022-12-09 12:19:19 +0100

    Apply PR comments

--- mcan/src/config.rs
+++ mcan/src/config.rs
@@ -222,13 +222,11 @@ pub enum Mode {
     /// data) is enabled. This does not prevent use of classic CAN frames.
     Fd {
         /// If `true`, FD frames can be transmitted with bit rate switching.
+        /// Otherwise, attempts to transmit FD frames will return errors.
         ///
         /// Regardless of this setting, data phase timing still must be
         /// configured as *reception* of bit-rate-switched messages is still
-        /// possible
-        ///
-        /// This setting must be `true` if construction of bit-rate-switched
-        /// message is to succeed.
+        /// possible.
         allow_bit_rate_switching: bool,
         /// Bit timing parameters for the data phase of bit rate switched FD
         /// frames.
